27a366f88525057cbdc6d6e59a81dcab219a72c0
ThomassMathisen <thomasmathisen@hotmail.com>
2022-09-29 16:48:01 +0200

    Add logic to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import './styles/App.css';
+import React, { useState } from "react";
+import Image from "./components/Image";
+import Header from "./components/Header";
+import "./styles/App.css";
+import Leaderboard from "./components/Leaderboard";
+import Confetti from "react-confetti";
 
 function App() {
-  return ()
+  const [startGame, setStartGame] = useState(false);
+  const [wonGame, setWonGame] = useState(false);
+  const [displayLeaderboard, setDisplayLeaderboard] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      {wonGame && !displayLeaderboard && (
+        <Confetti
+          width={1360}
+          height={1000}
+          numberOfPieces={500}
+          recycle={false}
+        />
+      )}
+      <Header
+        setDisplayLeaderboard={setDisplayLeaderboard}
+        displayLeaderboard={displayLeaderboard}
+        wonGame={wonGame}
+        counter={counter}
+        setCounter={setCounter}
+        startGame={startGame}
+        setWonGame={setWonGame}
+        setStartGame={setStartGame}
+      />
+      <main>
+        {!displayLeaderboard && (
+          <Image
+          setDisplayLeaderboard={setDisplayLeaderboard}
+          counter={counter}
+          wonGame={wonGame}
+          setWonGame={setWonGame}
+          startGame={startGame}
+          setStartGame={setStartGame}
+          />
+        )}
+        {displayLeaderboard && <Leaderboard/>}
+      </main>
+    </>
+  );
 }
 
 export default App;
